9962a54444d0102cf325952836b220511e010090
numbers <57849815+numberss@users.noreply.github.com>
2025-01-09 20:55:50 +0000

    updated, now horizontal

--- SineBeams.lua
+++ SineBeams.lua
@@ -1,5 +1,5 @@
 local tweenService = game:GetService("TweenService")
-local info = TweenInfo.new(2, Enum.EasingStyle.Circular, Enum.EasingDirection.Out)
+local info = TweenInfo.new(1, Enum.EasingStyle.Circular, Enum.EasingDirection.Out)
 
 local firstPart = script.Parent:WaitForChild("Part")
 local startPos = firstPart.Position
@@ -15,7 +15,7 @@ for i=1, steps do
 	local clone = firstPart:Clone()
 	clone.BrickColor = BrickColor.new("Cyan")
 	parts[i] = clone
-	clone.Parent = workspace
+	clone.Parent = script.Parent
 	clone.Position = newPos
 	Instance.new("Attachment", clone)
 	pos = newPos
@@ -24,10 +24,11 @@ for i=1, steps do
 	local beam = Instance.new("Beam", clone)
 	beam.Attachment0 = clone.Attachment
 	beam.Attachment1 = parts[i-1].Attachment
-	beam.Width0 = 20
-	beam.Width1 = 20
-	beam.Color = ColorSequence.new(Color3.fromRGB(255, 150, 255))
+	beam.Width0 = 10
+	beam.Width1 = 10
+	beam.Color = ColorSequence.new(Color3.fromRGB(255, 255, 255))
 	beam.Transparency = NumberSequence.new(0)
+	--beam.Texture = "rbxassetid://137760291120682"
 end
 
 local accum = 0
@@ -35,10 +36,10 @@ local db = 0
 game:GetService("RunService").Heartbeat:Connect(function(dt)
 	accum += dt
 	db += dt
-	if db >= 3 then
+	if db >= 1.5 then
 		for _,part in pairs(parts) do
 			local offset = math.random(0,75)/10 - 2.5
-			tweenService:Create(part, info, {Position = Vector3.new(part.Position.X, startPos.Y + offset, part.Position.Z)}):Play()
+			tweenService:Create(part, info, {Position = Vector3.new(startPos.X + offset, part.Position.Y, part.Position.Z)}):Play()
 		end
 		db = 0
 	end
